67f60bd730d6e7c20c77698bec1cbeb7df28b67f
no-name <no-email@email.com>
2023-06-27 20:09:09 +0530

    form and filterbutton componenet added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const DATA = [
+  { id: "todo-0", name: "Update Healthify", completed: true },
+  { id: "todo-1", name: "Create Technical Architecture", completed: false },
+  { id: "todo-2", name: "Create Database Design", completed: false },
+  { id: "todo-3", name: "Update the desktop design", completed: false },
+];
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App subject='Akash'/>
+    <App subject='Akash' tasks={DATA}/>
   </React.StrictMode>
 );
 
